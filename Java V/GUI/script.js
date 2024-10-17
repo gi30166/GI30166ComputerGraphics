5149189539f88259b6ed608aeae73b9b57d4fe88
= <ts30164@seeu.edu.mk>
2024-10-17 13:48:37 +0200

    Lab Assignment and added testing in folders

--- Java V/GUI/script.js
+++ Java V/GUI/script.js
@@ -1,9 +1,30 @@
 import * as THREE from 'three';
-
+import GUI from 'lil-gui'
 const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
+
+const gui = new GUI();
+const boxControls = gui.addFolder('Box Controls');
+
+gui.add (mesh.position,'y', -3, 3, 0.1).name('Position Y');
+gui.add (mesh.rotation,'z', -3, 3, 0.1).name('Rotation Z');
+gui.add (material, 'wireframe').name('WireFrame?');
+gui.addColor(material, 'color').name('Select Color');
+
+var rotateSpeed = 0;
+
+const startRotate = () => {
+    if (rotateSpeed != 0) {
+        rotateSpeed = 0;
+    } else {
+        rotateSpeed = 0.1;
+    }
+}
+
+gui.add({startRotate}, 'startRotate');
+
 scene.add(mesh)
 const sizes = {
     width: 800,
@@ -22,4 +43,9 @@ renderer.setSize(800, 600)
 document.getElementById("scene").appendChild(renderer.domElement);
 
 
-renderer.render(scene, camera)
+const animate = () =>{
+    requestAnimationFrame(animate)
+    renderer.render(scene,camera)
+}
+
+animate();
